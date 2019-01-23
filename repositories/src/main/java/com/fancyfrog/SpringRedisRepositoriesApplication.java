7e3fd93b57455007f7a72f26b855be1b218047d7
Ujjwal Gupta <ujjwal@quaqua.com>
2019-01-23 16:22:51 +0530

    Initial setup for multi module project

---
+++ repositories/src/main/java/com/fancyfrog/SpringRedisRepositoriesApplication.java
@@ -0,0 +1,12 @@
+package com.fancyfrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisRepositoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisRepositoriesApplication.class, args);
+    }
+}
